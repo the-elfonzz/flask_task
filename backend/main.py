641a2aa13e0ae1837fd3598e5f3195586a082972
Alexander <alexander.kostov89@gmail.com>
2019-02-24 18:57:48 +0200

    create 'flask_task' virtualenv; installed Flask; add create-reack-app instance

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask("__main__")
+
+
+@app.route("/")
+def my_index():
+    return render_template("index.html", token="Hello React")
+
+
+app.run(debug=True)
